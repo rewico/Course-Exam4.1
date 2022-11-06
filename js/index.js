aba576cf0551ba03f93687525b966fb93fc7ff9f
rewico <codergq@gmail.com>
2022-11-06 14:32:45 +0500

    added functionality of delete, changed styles of card

--- js/index.js
+++ js/index.js
@@ -6,20 +6,23 @@ const render = (data) => {
 
     elLi.classList = "card mx-2 my-3";
     elLi.style.width = "17rem";
+    elLi.dataset.id = element.id;
 
     elLi.innerHTML = `
     <div>
       <img
         src="${element.image}"
-        class="card-img-top"
+        class="card-img-top p-1"
         alt="${element.title}"
         height="200"
       />
       <div class="card-body">
-        <h5 class="card-title" style="height:144px">${element.title}</h5>
-        <p class="card-text desc">${element.description}</p>
-        <p class="card-text"><span>Price: </span>${element.price}</p>
-        <i class="material-symbols-rounded d-block text-end">delete</i>
+        <h5 class="card-title mb-3">${element.title}</h5>
+        <p class="card-text desc mb-2">${element.description}</p>
+        <p class="card-text"><span>Price: </span>${element.price}$</p>
+        <div class="d-flex justify-content-end">
+          <i class="material-symbols-rounded">delete</i>
+        </div>
       </div>
   </div>`;
 
@@ -32,3 +35,31 @@ const render = (data) => {
   const DATA = await RESPONSE.json();
   render(DATA);
 })();
+
+const seperate = (evt) => {
+  if (evt.target.matches("i")) {
+    if (confirm("Are you sure to delete?")) {
+      (async () => {
+        try {
+          const RESPONSE = await fetch(
+            `https://fakestoreapi.com/products/${
+              evt.target.closest("li").dataset.id
+            }`,
+            {
+              method: "DELETE",
+            }
+          );
+          const DATA = await RESPONSE.json();
+          if (DATA) {
+            evt.target.closest("li").remove();
+            alert("Successfully deleted!");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
+  }
+};
+
+elUl.addEventListener("click", seperate);
